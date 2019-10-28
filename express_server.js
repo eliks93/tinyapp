653f98961905260351896a435a9f0f2081346067
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-28 23:09:06 +0000

    added random string generator to generate 6 character long string

--- express_server.js
+++ express_server.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+// generates a random string
+function generateRandomString() {
+  let count = 0;
+  let string = ""
+  let randomNumbers = [];
+  const alphaNumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  while (count < 6) { 
+  randomNumbers.push(Math.round(Math.random() * 62))
+  count += 1;
+  }
+  for (const number of randomNumbers) {
+    string += alphaNumeric[number];
+  }
+  return string;
+}
+
+console.log(generateRandomString())
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -9,10 +29,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("pages/urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
+});
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
