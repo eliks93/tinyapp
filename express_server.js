74b36f9aadac8345b2ca2427aa96aaf628686e70
Eli Kittross-Schnell <eliks92@gmail.com>
2019-10-28 21:59:56 +0000

    added some additional functianilty using ejs. Placeholder pages and partials

--- express_server.js
+++ express_server.js
@@ -2,13 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 }
+];
+const tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+res.render('pages/index', {
+    drinks: drinks,
+    tagline: tagline
+});
 });
 
 app.get("/hello", (req, res) => {
@@ -22,3 +34,4 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
